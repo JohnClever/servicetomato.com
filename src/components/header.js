e61e0b2397bc3c8dcefd59c4b71dda8a7e2838b6
John Clever <jclever114@gmail.com>
2020-06-14 07:29:32 +0100

    eleven

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import 'materialize-css/dist/css/materialize.min.css'
 import logo from '../components/img/SERVICE TOMATO LOGO.jpg'
-import logoMenu from '../components/img/icon_menu.svg'
+
 
 class Navbar extends Component{
     render() {
@@ -9,9 +9,9 @@ class Navbar extends Component{
             <div>
                 <nav>
                     <div className="nav-wrapper">
-                    <li><a href="/" className="brand-logo nav-menu left">Servicetomato.com</a></li>
+                    <li><a href="/" className="brand-logo nav-link left">Servicetomato.com</a></li>
                     <a href="/" className="brand-logo center"><img src={logo} alt= "logo" width="15%"/></a>
-                    <a href="/" className="brand-logo right"><img src={logoMenu} alt="logoMenu"/></a>
+                    <a href="/" className="brand-logo nav-menu right">. . .</a>
                     </div>
                 </nav>
             </div>
