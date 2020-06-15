047e9433bd421c78c1e7cfbc2b8d34ec48102bdc
John Clever <jclever114@gmail.com>
2020-06-15 03:39:52 +0100

    fifteen

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import './App.css';
-// import {LoginForm} from './components/login_form'
-import Menu from './components/Menu'
-import {Navbar} from './components/header'
 import 'materialize-css/dist/css/materialize.min.css'
+import {BrowserRouter as Router, Route, Switch, Link, useParams} from 'react-router-dom'
+import 'materialize-css/dist/css/materialize.min.css'
+import logo from './components/img/SERVICE TOMATO LOGO.jpg'
+import logoMenu from './components/img/icon_menu.svg'
+import {Menu} from './components/Menu'
+import Home from './components/Home'
 
 function App() {
   return (
-    <div className="container">
-      <Navbar />
-      <Menu />
-      {/* <LoginForm /> */}
-    </div>
+   <Router>
+     <div>
+        <nav>
+          <div className="nav-wrapper">
+              <Link to="/" className="brand-logo nav-link left">Servicetomato.com</Link>
+              <Link to="/" className="brand-logo nav-logo center"><img src={logo} alt= "logo" width="100%"/></Link>
+              <Link to="/menu" className="brand-logo nav-menu right"><img src={logoMenu} alt= "logo"/></Link>
+          </div>
+        </nav>
+      </div>
+     <Switch>
+      <Route exact path ="/" component={Home}></Route>
+      <Route path="/menu" component={Menu}></Route>
+      <Route path="/menu/:login" component={Menu}></Route>
+     </Switch>
+   </Router>
   );
 }
+
 
 export default App;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react'
+import { Link, BrowserRouter as Router, Switch, Route, useParams, useRouteMatch} from 'react-router-dom'
+import { LoginForm } from './login_form'
 
 class Menu extends Component {
-    render() {
+    render() {  
+     
         return (
+            <Router>             
             <div className="menu">
                 <div className="items">
                     <ul>
-                        <li><a href="/">JOIN</a></li>
-                        <li><a href="/">SIGN IN</a></li>
-                        <li><a href="/">POST A SERVICE</a></li>
-                        <li><a href="/">POST A JOB</a></li>
+                        <li><Link >JOIN</Link></li>
+                        <li><Link to="/login">SIGN IN</Link></li>
+                        <li><Link >POST A SERVICE</Link></li>
+                        <li><Link >POST A JOB</Link></li>
                     </ul>
                     <hr />
-                    <p><a href="/">Share a story</a></p>
+                    <p><Link >Share a story</Link></p>
                 </div>
+            </div>
+           
+            </Router>
+         
+        )
+    }
+};
+
+export default class Menus extends Component {
+    render() {
+        let {menuId} = useParams()
+        return (
+            <div>
+                {menuId}
             </div>
         )
     }
-}
+};
 
-export default Menu
+
+export  {Menu}
